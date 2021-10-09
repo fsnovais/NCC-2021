3dd27629526d5755d051d471b6a3a600daf95e0a
lancaroline <lana.de.sa@hotmail.com>
2021-10-09 18:07:07 -0300

    Estrutura da página Nossos Objetivos

--- src/components/objectives/Objectives.jsx
+++ src/components/objectives/Objectives.jsx
@@ -3,7 +3,39 @@ import "./objectives.scss"
 export default function Objectives() {
     return (
         <div className="objectives">
-            Objectives
+            <h1>
+                Nossos objetivos
+            </h1>
+            <text>
+                Investir na formação e desenvolvimento de talentos para participar de competições de programação e computação, contribuindo para melhor qualificá-los para o mercado de trabalho de alta tecnologia, assim como aumentar a visibilidade do UniCEUB em tais competições.
+            </text>
+
+            <div className='GridIdeia'>
+                <div>
+                    <img src="assets/lampada.png" alt="Lâmpada" />
+                    <tex>
+                        Promover a troca de experiências acadêmicas, por meio da interação dos nossos alunos com alunos de outras universidades, como resultado das participações nas competições nacionais e internacionais.
+                    </tex>
+                </div>
+                <div>
+                    <img src="assets/lampada.png" alt="Lâmpada" />
+                    <tex>
+                        Formar recursos humanos qualificados e experientes em técnicas avançadas de programação, incluindo tópicos não cobertos pelo currículo básico dos cursos de computação.
+                    </tex>
+                </div>
+                <div>
+                    <img src="assets/lampada.png" alt="Lâmpada" />
+                    <tex>
+                        Melhorar a visibilidade do UniCEUB em competições de programação de alto nível, reforçando assim o compromisso de busca pela excelência no ensino da instituição.
+                    </tex>
+                </div>
+                <div>
+                    <img src="assets/lampada.png" alt="Lâmpada" />
+                    <tex>
+                        Fomentar a participação dos alunos em competições de programação de alto nível, nacionais e internacionais, como um recurso de formação complementar para desenvolver habilidades de raciocínio lógico, colaboração e trabalho em equipe, e desenvolvimento de soluções dentro de prazos bem definidos.
+                    </tex>
+                </div>
+            </div>
         </div>
     )
 }
