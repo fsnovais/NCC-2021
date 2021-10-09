210b5738b4e89f12b269408460a42fa5aaf9e255
Felipe <felipekun213@gmail.com>
2021-10-09 18:08:14 -0300

    alteração na seção about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,13 +3,13 @@ import "./about.scss"
 export default function About() {
     return (
         <div className="about">
-            <h4>
-                O que é?
-            </h4>
-            <div className='paragrafo'>
-                <text>
-                    O  <text style={{ fontWeight: "bold" }}>“Núcleo de Computação Competitiva (NCC)”</text> é um projeto da seleção de programação competitiva do <text style={{ fontWeight: "bold" }}>UniCEUB</text>,<br /> sob curadoria do prof Aderbal Botelho, que visa fomentar nos alunos da graduação e <br />pós-graduação, a cultura de participação em competições de programação, fomentando o <br />desenvolvimento técnico, a capacidade de raciocínio lógico, e a colaboração e trabalho em grupo <br />para a resolução de problemas computacionais.
-                </text>
+            <div className="title">
+           <h1> O que é?</h1>
+            </div>
+            <div className="text">
+
+            O <text style={{fontWeight: "bold"}}>“Núcleo de Computação Competitiva (NCC)”</text> é um projeto da seleção de programação competitiva do <text style={{fontWeight: "bold"}}>UniCEUB</text>, sob curadoria do prof Aderbal Botelho, que visa fomentar nos alunos da graduação e pós-graduação, a cultura de participação em competições de programação, fomentando o desenvolvimento técnico, a capacidade de raciocínio lógico, e a colaboração e trabalho em grupo para a resolução de problemas computacionais.
+
             </div>
         </div>
     )
