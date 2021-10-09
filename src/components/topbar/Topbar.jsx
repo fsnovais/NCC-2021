75619d0e94d25622c4f5e290450f3952d3a78447
Felipe <felipekun213@gmail.com>
2021-10-09 13:20:50 -0300

    alteração na topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,21 @@ import "./topbar.scss"
 export default function Topbar() {
     return (
         <div className="topbar">
-            Topbar
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro">
+                        <div className="imgContainer">
+                        <img src="assets/logo.png" alt="logo" />
+                        </div>
+                    </a>
+                </div>
+                <div className="right">
+                    <a href="#">Home</a>
+                    <a href="#">ICPC</a>
+                    <a href="#">Sites de interesse</a>
+                    <a href="#">Inscreva-se</a>
+                </div>
+            </div>
         </div>
     )
 }
