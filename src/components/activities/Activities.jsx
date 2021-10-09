3c5fb454a55c07dd36ba45a494e54c20089fc089
lancaroline <lana.de.sa@hotmail.com>
2021-10-09 18:44:26 -0300

    Estrutura da página Atividades

--- src/components/activities/Activities.jsx
+++ src/components/activities/Activities.jsx
@@ -4,7 +4,64 @@ import "./activities.scss"
 export default function Activities() {
     return (
         <div className="activities">
-            Activities
+            <h1>Atividades</h1>
+            <div className='GridLaptop'>
+                <div className='Linha1'>
+                <div>
+                    <div>
+                        <img src="assets/laptop.png" alt="Lâmpada" />
+                    </div>
+                    <div>
+                        <tex>
+                            Competições de programação regulares entre alunos
+                        </tex>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="assets/laptop.png" alt="Lâmpada" />
+                    </div>
+                    <div>
+                        <tex>
+                            Aulas sobre tópicos avançados de programação
+                        </tex>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="assets/laptop.png" alt="Lâmpada" />
+                    </div>
+                    <div>
+                        <tex>
+                            Treinamento regular de resolução de problemas de programação
+                        </tex>
+                    </div>
+                </div>
+                </div>
+                <div className='Linha2'>
+                <div>
+                    <div>
+                        <img src="assets/laptop.png" alt="Lâmpada" />
+                    </div>
+                    <div>
+                        <tex>
+                            Participação das edições regulares da Maratona de Programação da SBC - Sociedade Brasileira de Computação
+
+                        </tex>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="assets/laptop.png" alt="Lâmpada" />
+                    </div>
+                    <div>
+                        <tex>
+                            Sediar etapa(s) da Maratona de Programação da SBC - Sociedade Brasileira de Computação
+                        </tex>
+                    </div>
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
