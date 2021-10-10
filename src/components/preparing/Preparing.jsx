65e988524a1d4aa944320fa5f4ab861dda5c8adf
Felipe <felipekun213@gmail.com>
2021-10-09 21:55:40 -0300

    alteração na seção preparing

--- src/components/preparing/Preparing.jsx
+++ src/components/preparing/Preparing.jsx
@@ -3,7 +3,22 @@ import "./preparing.scss"
 export default function Preparing() {
     return (
         <div className="preparing">
-            Preparing
+            <div className="title">
+                <h1> Me interessei! Como participo?</h1>
+            </div>
+            <div className="container">
+                <div className="text">
+                    A seleção para  a  XXV Maratona de Programação da SBC, evento promovido pela SBC (Sociedade Brasileira de Computação) que é fase classificatória obrigatória para a ICPC GLOBAL acontecerá de forma online dia 28 de outubro de 2020 às 15:00 horário de Brasília, objetivando formar ao menos um time com três participantes do UniCEUB. Dependendo do desempenho dos alunos, mais de uma equipe (até o limite de 5) poderão ser formadas. Para acompanhar um guia de referência para se preparar acesse:
+            </div>
+                <div className="buttons">
+                    <a href="#">
+                        <button>Seletiva para Maratona de Programação</button>
+                    </a>
+                    <a href="#">
+                        <button>Desafios Propostos</button>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
