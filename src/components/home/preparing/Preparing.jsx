a8f0d20da29f10af4452f3602edc02d945f74e29
Felipe <felipekun213@gmail.com>
2021-10-12 12:43:51 -0300

    correção do texto

--- src/components/home/preparing/Preparing.jsx
+++ src/components/home/preparing/Preparing.jsx
@@ -8,7 +8,7 @@ export default function Preparing() {
             </div>
             <div className="container">
                 <div className="text">
-                    A seleção para  a  XXV Maratona de Programação da SBC, evento promovido pela SBC (Sociedade Brasileira de Computação) que é fase classificatória obrigatória para a ICPC GLOBAL acontecerá de forma online dia 28 de outubro de 2020 às 15:00 horário de Brasília, objetivando formar ao menos um time com três participantes do UniCEUB. Dependendo do desempenho dos alunos, mais de uma equipe (até o limite de 5) poderão ser formadas. Para acompanhar um guia de referência para se preparar acesse:
+                    AA seleção para  para participar de um dos times aptos a participar  tanto dos eventos de programação competitiva como das competições de segurança da informação podem ser realizadas através do formulário clicando em inscreva-se. após isso, você receberá um email com instruções para acesso às plataformas de treinamento e seleção, Boca (programação competitiva ou CTFd (segurança da informação).
             </div>
                 <div className="buttons">
                     <a href="https://boca.ceubos.com.br/boca" target="_blank" rel="noreferrer">
